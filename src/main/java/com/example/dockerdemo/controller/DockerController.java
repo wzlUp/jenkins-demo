d162f152475e7ad851172ad2877283326c3e8690
HFDPI6BDA6T4MT5\Administrator <1076500039@qq.com>
2020-09-23 15:00:54 +0800

    first commit

---
+++ src/main/java/com/example/dockerdemo/controller/DockerController.java
@@ -0,0 +1,15 @@
+package com.example.dockerdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docker")
+public class DockerController {
+
+    @GetMapping(value = "/dockerTest")
+    public String dockerTest() {
+        return "docker test ok ";
+    }
+}
